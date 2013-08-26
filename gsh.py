1fabdc3ab6a1d393e1718071578e0e75bf33fd43
Gary M. Josack <gary@byoteki.com>
2013-08-25 23:13:02 -0700

    Fixes and Iteration
    Removed select code and changed to queue to fix race condition
    Fixed join of pool which simplified fork code and removed a different race condition
    Add commandline for parsing files
    Remove usage of gevent-subprocess since I ended up writing my own streaming code

--- gsh.py
+++ gsh.py
@@ -1,83 +1,76 @@
 #!/usr/bin/env python
 
-import pipes
 import sys
 
 import gevent
-from gevent.select import select, error as select_error
 from gevent.pool import Pool
+from gevent.queue import Queue, Empty
 
-try:
-    from gevent.subprocess import Popen, PIPE
-    # gevent.subprocess only exists in gevent-1.0 which isn't
-    # available on PyPI yet. Fall back to gevent_subprocess
-    # if it's not available.
-except ImportError:
-    try:
-        from gevent_subprocess import Popen, PIPE
-        # If this library isn't installed either just fall back to
-        # the stdlib subprocess module. It'll be slower but still
-        # work fairly well.
-    except ImportError:
-        from subprocess import Popen, PIPE
-
-
-def quote_for_ssh(args):
-    return " ".join(pipes.quote(s) for s in args)
+from subprocess import Popen, PIPE
 
 
 def remote_command(hostname, command, output_callbacks=None):
-    proc = Popen(["ssh", hostname] + command, stdout=PIPE, stderr=PIPE)
+    proc = Popen(["ssh", hostname] + command, stdout=PIPE, stderr=PIPE, bufsize=1)
 
-    output_fds = {
-        proc.stdout: dict(name="stdout", finished=False),
-        proc.stderr: dict(name="stderr", finished=False),
+    names = {
+        proc.stdout: "stdout",
+        proc.stderr: "stderr",
     }
 
-    while not all([output_fds[proc.stdout]["finished"],
-                   output_fds[proc.stdout]["finished"]]):
-        read_fds = [key for key, val in output_fds.iteritems() if not val["finished"]]
-        read_fds = select(read_fds, [], [], .2)[0]
-        for read_fd in read_fds:
-            output = read_fd.readline()
-            if not output:
-                output_fds[read_fd]["finished"] = True
-            if output:
-                #print hostname, output_fds[read_fd]["name"], output,
-                print "%s: %s" % (hostname, output)
+    output_queue = Queue()
 
-        if proc.poll() is not None:
-            print hostname, "finished"
-            break
-        print hostname, "next run"
+    def stream_fd(fd, queue):
+        for line in iter(fd.readline, b""):
+            queue.put_nowait((fd, line))
 
-    return
+    def consume(queue):
+        while True:
+            try:
+                output = queue.get()
+            except Empty:
+                continue
 
+            # None is explicitly sent to shutdown the consumer
+            if output is None:
+                return
+
+            fd, line = output
+            print "%s(%s): %s" % (hostname, names[fd], line),
+
+
+    out_ev = gevent.spawn(stream_fd, proc.stdout, output_queue)
+    err_ev = gevent.spawn(stream_fd, proc.stderr, output_queue)
+    consumer = gevent.spawn(consume, output_queue)
+
+    gevent.joinall([out_ev, err_ev])
+    output_queue.put_nowait(None)
+    consumer.join()
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser(description="Run a command across many machines.")
     parser.add_argument("command", nargs="*", default=None, help="Command to execute remotely.")
+    parser.add_argument("-f", "--file", default=[], action="append", help="Use the file as list of machines.")
     parser.add_argument("-F", "--forklimit", default=64, type=int, help="Limit on concurrenct processes.")
     args = parser.parse_args()
 
-    hosts = [line.strip() for line in open("hosts")]
+    hosts = []
+    for host_file in args.file:
+        hosts.extend([line.strip() for line in open(host_file)])
 
     if not args.command or not any(args.command):
         print "\n".join(hosts)
         sys.exit()
 
+
+    greenlets = []
     try:
-        if args.forklimit == 1:
-            for host in hosts:
-                remote_command(host, args.command)
-        else:
             gsh_pool = Pool(args.forklimit)
 
             for host in hosts:
-                gsh_pool.apply_async(remote_command, (host, args.command))
-
+                greenlets.append(gsh_pool.apply_async(remote_command, (host, args.command)))
+            gevent.joinall(greenlets)
             gsh_pool.join()
     except KeyboardInterrupt:
         sys.exit("Bye")
