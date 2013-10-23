6bce2f2eb5311f994873154061b2ac2c54f4047d
Gary M. Josack <gary@byoteki.com>
2013-10-22 20:32:56 -0700

    Fix traceback when pipe is closed

--- gsh/plugins/hooks/printer_hook.py
+++ gsh/plugins/hooks/printer_hook.py
@@ -2,12 +2,30 @@
 import sys
 from gsh.plugin import BaseExecutionHook
 
-class PrinterHook(BaseExecutionHook):
-    def update_host(self, hostname, stream, line):
+def update_host_handler(hostname, stream, line, prepend_host=False):
         if stream not in ("stdout", "stderr"):
             return
         writer = getattr(sys, stream)
-        writer.write(line)
-        writer.flush()
+        if prepend_host:
+            line = "%s: %s" % (hostname, line)
+        try:
+            writer.write(line)
+            writer.flush()
+        except IOError as err:
+            if writer is not sys.stderr:
+                output = "Failed to write to stream %s: (%s, %s)\n" % (stream, err.errno, err.strerror)
+                if prepend_host:
+                    output = "%s: %s" % (hostname, output)
+                sys.stderr.write(output)
+
+
+class PrinterHook(BaseExecutionHook):
+    def update_host(self, hostname, stream, line):
+        return update_host_handler(hostname, stream, line)
+
+
+class MachinePrinterHook(BaseExecutionHook):
+    def update_host(self, hostname, stream, line):
+        return update_host_handler(hostname, stream, line, prepend_host=True)
 
 
