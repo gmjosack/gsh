0107de09f6a4dd844cc1ae4c6023fa45df99f63a
Gary M. Josack <gary@byoteki.com>
2013-10-25 00:04:05 -0700

    Have machine printer line up output nicely.

--- gsh/plugins/hooks/printer_hook.py
+++ gsh/plugins/hooks/printer_hook.py
@@ -2,31 +2,39 @@
 import sys
 from gsh.plugin import BaseExecutionHook
 
-def update_host_handler(hostname, stream, line, prepend_host=False):
+
+class PrinterHook(BaseExecutionHook):
+    def __init__(self, *args, **kwargs):
+        self.prepend_host = False
+        self.longest_len = 0
+        super(PrinterHook, self).__init__(*args, **kwargs)
+
+    def pre_job(self, command, hosts, timestamp):
+        # Add one to account for :
+        self.longest_len = len(max(hosts, key=len)) + 1
+
+    def update_host(self, hostname, stream, line):
         if stream not in ("stdout", "stderr"):
             return
         writer = getattr(sys, stream)
         line = line.decode('utf-8')
-        if prepend_host:
-            line = "%s: %s" % (hostname, line)
+
         try:
-            writer.write(line)
-            writer.flush()
+            self._write(writer, hostname, line)
         except IOError as err:
             if writer is not sys.stderr:
                 output = "Failed to write to stream %s: (%s, %s)\n" % (stream, err.errno, err.strerror)
-                if prepend_host:
-                    output = "%s: %s" % (hostname, output)
-                sys.stderr.write(output)
+                self._write(sys.stderr, hostname, output)
+
+    def _write(self, stream, hostname, line):
+        if self.prepend_host:
+            line = "%%-%ss %%s" % self.longest_len % (hostname + ":", line)
+        stream.write(line)
+        stream.flush()
 
 
-class PrinterHook(BaseExecutionHook):
-    def update_host(self, hostname, stream, line):
-        return update_host_handler(hostname, stream, line)
 
-
-class MachinePrinterHook(BaseExecutionHook):
-    def update_host(self, hostname, stream, line):
-        return update_host_handler(hostname, stream, line, prepend_host=True)
-
-
+class MachinePrinterHook(PrinterHook):
+    def __init__(self, *args, **kwargs):
+        self.prepend_host = True
+        super(PrinterHook, self).__init__(*args, **kwargs)
