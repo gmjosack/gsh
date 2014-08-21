1806683a5a6a2ed2834137ec761fb2c3b6bad788
David Mah <MahHaha@gmail.com>
2014-08-21 11:26:16 -0700

    Import run_remote_command into gsh __init__

--- gsh/__init__.py
+++ gsh/__init__.py
@@ -3,6 +3,7 @@ warnings.simplefilter("ignore")
 
 from .core import RemotePopen, Gsh
 from .version import __version__
+from .util import run_remote_command
 
 __all__ = ["RemotePopen", "Core"]
 __author__ = "Gary M. Josack <gary@byoteki.com>"
